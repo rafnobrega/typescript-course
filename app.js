1d19ff899b2a5060715ca532966c0ef4d46fe270
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-26 14:21:05 -0700

    review union-aliases

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
+function combine(input1, input2, resultConversion) {
+    var result;
+    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
-        return result;
+        result = input1.toString() + input2.toString();
     }
+    return result;
+    // if (resultConversion === "as-number") {
+    //   return +result;
+    // } else {
+    //   return result;
+    // }
 }
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-add(number1, number2, printResult, resultPhrase);
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+var combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
