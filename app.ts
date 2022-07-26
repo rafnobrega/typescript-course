b306ab523b33017e7a7ac7d29cbdf7d793bb4536
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-25 17:31:08 -0700

    types

--- app.ts
+++ app.ts
@@ -1,3 +1,15 @@
-console.log("Your code goes here!");
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
 
-// tsc app.ts
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+add(number1, number2, printResult, resultPhrase);
