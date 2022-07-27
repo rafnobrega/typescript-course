7c5eb0caac658d9248b7ac05e18db28d7c582f63
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-27 11:06:59 -0700

    create functions.ts file

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === "as-number") {
-    //   return +result;
-    // } else {
-    //   return result;
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-var combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-var combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+function printResult(num) {
+    console.log("Result: " + num);
+}
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+console.log(combineValues(8, 8));
+// let someValue: undefined;
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
